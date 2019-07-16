c7dd19788ab815c33a598cfd9bed43b6669a8c65
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 22:11:24 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,12 @@
+XX=int(input())
+YY=list(map(str,input().split()))[:Xx]
+ltl=[]
+tt=[]
+for i in range(0,len(YY)):
+    l.append(YY[i].lower())
+for i in range(0,XX):
+      m=min(l)
+      tt.append(m)
+      l.remove(min(l))
+for i in t:
+    print(i)
