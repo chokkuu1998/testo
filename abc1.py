ae7c6de9f1b0f119943b091dedbce3db60d240ba
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-17 14:24:36 +0530

    Create abc1.py

---
+++ abc1.py
@@ -0,0 +1,6 @@
+a=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+nn=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+gg=(input())
+for o in range(20):
+    if(gg==mm[o]):
+        print(nn[o])
