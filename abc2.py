49619c19ae4bf9c152cf87e6a576ceb6e63800c0
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-17 14:26:19 +0530

    Create abc2.py

---
+++ abc2.py
@@ -0,0 +1,7 @@
+njj1=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+njj2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+njj3=str(input())
+for njj4 in range(20):
+  if(njj3==njj1[njj4]):
+    print(njj2[njj4])
+    break
